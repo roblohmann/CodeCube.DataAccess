3fb29328d19573fdbfcd8ef755ff9bcdcc1c30ec
Rob Lohmann <roblohman@gmail.com>
2019-12-02 16:28:48 +0100

    refactoring and comments

--- CodeCube.DataAccess.EntityFrameworkCore.Sql/Extensions/ModelBuilderExtensions.cs
+++ CodeCube.DataAccess.EntityFrameworkCore.Sql/Extensions/ModelBuilderExtensions.cs
@@ -10,7 +10,25 @@ namespace CodeCube.DataAccess.EntityFrameworkCore.Sql.Extensions
     //Code taken from Phil Haacked > https://haacked.com/archive/2019/07/29/query-filter-by-interface/
     public static class ModelBuilderExtensions
     {
-        static void SetQueryFilter<TEntity, TEntityInterface>(this ModelBuilder builder, Expression<Func<TEntityInterface, bool>> filterExpression)
+        /// <summary>
+        /// Apply the specified expression for the matching interface on all entities
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="filterExpression">The filter expression to apply.</param>
+        /// <typeparam name="TEntityInterface"></typeparam>
+        public static void SetQueryFilterOnAllEntities<TEntityInterface>(this ModelBuilder builder, Expression<Func<TEntityInterface, bool>> filterExpression)
+        {
+            var entityTypes = builder.Model.GetEntityTypes()
+                                .Where(t => t.BaseType == null)
+                                .Select(t => t.ClrType)
+                                .Where(t => typeof(TEntityInterface).IsAssignableFrom(t));
+
+            foreach (var type in entityTypes)
+            {
+                builder.SetEntityQueryFilter(type, filterExpression);
+            }
+        }
+        private static void SetQueryFilter<TEntity, TEntityInterface>(this ModelBuilder builder, Expression<Func<TEntityInterface, bool>> filterExpression)
                 where TEntityInterface : class
                 where TEntity : class, TEntityInterface
         {
@@ -18,53 +36,34 @@ namespace CodeCube.DataAccess.EntityFrameworkCore.Sql.Extensions
             builder.Entity<TEntity>().HasQueryFilter(concreteExpression);
         }
 
-        static readonly MethodInfo SetQueryFilterMethod = typeof(ModelBuilderExtensions)
+        private static readonly MethodInfo SetQueryFilterMethod = typeof(ModelBuilderExtensions)
                 .GetMethods(BindingFlags.NonPublic | BindingFlags.Static)
                 .Single(t => t.IsGenericMethod && t.Name == nameof(SetQueryFilter));
 
-        static void SetEntityQueryFilter<TEntityInterface>(
-                this ModelBuilder builder,
-                Type entityType,
-                Expression<Func<TEntityInterface, bool>> filterExpression)
+        private static void SetEntityQueryFilter<TEntityInterface>(this ModelBuilder builder, Type entityType, Expression<Func<TEntityInterface, bool>> filterExpression)
         {
             SetQueryFilterMethod
                     .MakeGenericMethod(entityType, typeof(TEntityInterface))
                     .Invoke(null, new object[] { builder, filterExpression });
         }
 
-        public static void SetQueryFilterOnAllEntities<TEntityInterface>(
-                this ModelBuilder builder,
-                Expression<Func<TEntityInterface, bool>> filterExpression)
-        {
-            foreach (var type in builder.Model.GetEntityTypes()
-                    .Where(t => t.BaseType == null)
-                    .Select(t => t.ClrType)
-                    .Where(t => typeof(TEntityInterface).IsAssignableFrom(t)))
-            {
-                builder.SetEntityQueryFilter<TEntityInterface>(
-                        type,
-                        filterExpression);
-            }
-        }
-
         /// <summary>
         /// 
         /// </summary>
-        /// <seealso cref="https://stackoverflow.com/a/45097532/291293"/>
         /// <param name="modelBuilder"></param>
         /// <param name="entityType"></param>
+        /// <remarks>This code has been kindly borrowed from https://stackoverflow.com/a/45097532/291293</remarks>
         public static void SetSoftDeleteFilter(this ModelBuilder modelBuilder, Type entityType)
         {
-            SetSoftDeleteFilterMethod.MakeGenericMethod(entityType)
-                    .Invoke(null, new object[] { modelBuilder });
+            SetSoftDeleteFilterMethod.MakeGenericMethod(entityType).Invoke(null, new object[] { modelBuilder });
         }
 
         /// <summary>
         /// 
         /// </summary>
-        /// <seealso cref="https://stackoverflow.com/a/45097532/291293"/>
         /// <typeparam name="TEntity"></typeparam>
         /// <param name="modelBuilder"></param>
+        /// <remarks>This code has been kindly borrowed from https://stackoverflow.com/a/45097532/291293</remarks>
         public static void SetSoftDeleteFilter<TEntity>(this ModelBuilder modelBuilder) where TEntity : class, ISoftDelete
         {
             modelBuilder.Entity<TEntity>().HasQueryFilter(x => !x.IsDeleted);

e698c9ae942926a9f88947155784553f3fa2a6f5
Rob Lohmann <rob@ecare.nl>
2020-04-14 16:34:27 +0200

    errorconstants updated

--- CodeCube.DataAccess.EntityFrameworkCore/Constants/ErrorConstants.cs
+++ CodeCube.DataAccess.EntityFrameworkCore/Constants/ErrorConstants.cs
@@ -2,7 +2,8 @@
 {
     public static class ErrorConstants
     {
-        public const string MissingConnectionstring = "There is not connectionstring configured. Please add a connectionstring with the name 'DatabaseConnection'.";
+        public const string MissingConnectionstring = "No connectionstring found!. Please add a connectionstring with the name 'DatabaseConnection' to your AppSettings.json or implement the method 'GetConnectionstring'";
+        public const string ConnectionstringParameterRequired = "The parameter 'connectionString' is required!";
         public const string ModelbuilderRequired = "ModelBuilder is required!";
         public const string AppSettingsBaseDirectoryNotFound = "Basedirectory for 'appsettings.json' could not be determined!";
         public const string AppSettingsBaseDirectoryNotFoundRecursively = "Basedirectory for 'appsettings.json' could not be determined! Paths have been recusively checked!";

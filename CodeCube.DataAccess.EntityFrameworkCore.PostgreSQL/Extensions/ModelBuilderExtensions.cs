5ea5c6c5728014dcee1d3856c74c749b80f0750d
Rob Lohmann <rob@ecare.nl>
2020-04-28 13:54:41 +0200

    added extension method to use UUID or Guid for a primary key on PostgreSQL

---
+++ CodeCube.DataAccess.EntityFrameworkCore.PostgreSQL/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using CodeCube.DataAccess.EntityFrameworkCore.Entities.PostgreSQL;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeCube.DataAccess.EntityFrameworkCore.PostgreSQL.Extensions
+{
+    /// <summary>
+    /// Class with extension methods on the EF Core ModelBuilder.
+    /// <emarks>PostgreSQL only!</emarks>
+    /// </summary>
+    public static class ModelBuilderExtensions
+    {
+        /// <summary>
+        /// Define the Id property of the specified <typeparamref name="TEntity"/> as an UUID / Guid in the database.
+        /// </summary>
+        /// <typeparam name="TEntity">The entity type.</typeparam>
+        /// <param name="modelBuilder">The EF Core ModelBuilder</param>
+        public static void HasKeyUniqueIdentifier<TEntity>(this ModelBuilder modelBuilder) where TEntity : BaseEntity
+        {
+            modelBuilder.HasPostgresExtension("uuid-ossp").Entity<TEntity>().Property(t => t.Id).HasDefaultValueSql("uuid_generate_v4()");
+        }
+    }
+}

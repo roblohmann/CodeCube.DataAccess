bce990c889cd325973d319f2f688cdbc861d423d
Rob Lohmann <rob@ecare.nl>
2020-05-26 08:57:48 +0200

    WIP - repository pattern

---
+++ CodeCube.DataAccess.EntityFrameworkCore.Repository/BaseRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeCube.DataAccess.EntityFrameworkCore.Repository
+{
+    public abstract class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : BaseEntity
+    {
+        protected ApplicationDbContext Context;
+
+        private DbSet<TEntity> Entities;
+
+        public BaseRepository(ApplicationDbContext applicationDbContext)
+        {
+            Context = applicationDbContext;
+            Entities = Context.Set<TEntity>();
+        }
+
+        public IQueryable<TEntity> Find()
+        {
+            return Entities.AsNoTracking();
+        }
+
+        public async Task<TEntity> Find(Guid id)
+        {
+            return await Entities.AsNoTracking().SingleOrDefaultAsync(o => o.Id == id);
+        }
+
+        public IQueryable<TEntity> Find(Expression<Func<TEntity, bool>> expression)
+        {
+            return Entities.AsNoTracking().Where(expression);
+        }
+
+        public async Task<TEntity> Get(Guid id)
+        {
+            return await Entities.SingleOrDefaultAsync(o => o.Id == id);
+        }
+
+        public async Task Add(TEntity entity)
+        {
+            await Entities.AddAsync(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            Entities.Update(entity);
+        }
+
+        public async Task Delete(Guid id)
+        {
+            TEntity entityToDelete = await Entities.SingleOrDefaultAsync(o => o.Id == id);
+            if (entityToDelete == null) return;
+
+            Delete(entityToDelete);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            Entities.Remove(entity);
+        }
+
+        public int Save()
+        {
+            return Context.SaveChanges();
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await Context.SaveChangesAsync();
+        }
+    }
+}
---
+++ CodeCube.DataAccess.EntityFrameworkCore.Repository/IBaseRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeCube.DataAccess.EntityFrameworkCore.Repository
+{
+    internal interface IBaseRepository<TEntity>
+    {
+        IQueryable<TEntity> Find();
+        Task<TEntity> Find(Guid id);
+        IQueryable<TEntity> Find(Expression<Func<TEntity, bool>> expression);
+        Task<TEntity> Get(Guid id);
+        Task Add(TEntity entity);
+        void Update(TEntity entity);
+        Task Delete(Guid id);
+        void Delete(TEntity entity);
+
+        int Save();
+        Task<int> SaveAsync();
+    }
+}

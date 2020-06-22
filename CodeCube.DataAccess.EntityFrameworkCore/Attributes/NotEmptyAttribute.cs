30bf383fe2ad903cad2fde714fef639ae1f7f129
Rob Lohmann <roblohman@gmail.com>
2020-06-22 10:52:26 +0200

    added not empty attribute

---
+++ CodeCube.DataAccess.EntityFrameworkCore/Attributes/NotEmptyAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeCube.DataAccess.EntityFrameworkCore.Attributes
+{
+    /// <summary>
+    /// Custom attribute to require the value not to be empty or have it's default value.
+    /// Eg. DateTime.MinValue and Guid.Empty are not allowed.
+    /// <remarks>Code is gracefully borrowed from https://andrewlock.net/creating-an-empty-guid-validation-attribute/</remarks>
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter)]
+    public sealed class NotEmptyAttribute : ValidationAttribute
+    {
+        public const string DefaultErrorMessage = "The {0} field must not be empty";
+        public NotEmptyAttribute() : base(DefaultErrorMessage) { }
+
+        public override bool IsValid(object value)
+        {
+            //NotDefault doesn't necessarily mean required
+            if (value is null)
+            {
+                return true;
+            }
+
+            var type = value.GetType();
+            if (type.IsValueType)
+            {
+                var defaultValue = Activator.CreateInstance(type);
+                return !value.Equals(defaultValue);
+            }
+
+            // non-null ref type
+            return true;
+        }
+    }
+}

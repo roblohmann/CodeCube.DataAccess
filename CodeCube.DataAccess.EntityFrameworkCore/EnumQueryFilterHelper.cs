45118465b49f94457103c94c5cf9951da31648c8
Rob Lohmann <roblohman@gmail.com>
2024-01-18 14:10:12 +0100

    add query filter helper

---
+++ CodeCube.DataAccess.EntityFrameworkCore/EnumQueryFilterHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeCube.DataAccess.EntityFrameworkCore
+{
+    public sealed class EnumQueryFilterHelper<T> where T : struct
+    {
+        public static IEnumerable<T> ApplyFilters(string[] filters)
+        {
+            var result = new List<T>();
+
+            if (filters.Length == 0)
+            {
+                foreach (var filter in (T[])Enum.GetValues(typeof(T)))
+                {
+                    result.Add(filter);
+                }
+            }
+            else
+            {
+                foreach (var filter in filters)
+                {
+                    if (Enum.TryParse<T>(filter, out var parsedFilter))
+                    {
+                        result.Add(parsedFilter);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
